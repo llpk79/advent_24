77070f1a300d4721b04e94a373007c88a9848506
Paul Kutrich <pkutrich@gmail.com>
2024-12-12 13:02:59 -0800

    Add day 3

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 // use crate::day_1::part_1::part_1;
 // use crate::day_1::part_2::part_2;
 // use crate::day_2::part_1::part_1;
-use crate::day_2::part_2::part_2;
+// use crate::day_2::part_2::part_2;
+// use crate::day_3::part_1::part_1;
+use crate::day_3::part_2::part_2;
 
 mod day_1;
 mod day_2;
+mod day_3;
 
 fn main() {
     part_2();
